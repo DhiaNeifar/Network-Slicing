9310f4459a631c7db453174933de18dd28f083ba
dhian <dhia.neifar@gmail.com>
2024-01-10 03:26:57 -0500

    Comprehensible and plain results at the end.

--- MILP.py
+++ MILP.py
@@ -2,24 +2,37 @@ import numpy as np
 import pulp
 
 
+def _print(text, value):
+    print(f'\n{text}:\n{value}')
+
+
 def generate_random_values(low, high, size):
     return np.random.randint(low=low, high=high, size=size, dtype=np.int16)
 
 
-def test_solution(_solution, _required_cpus, _total_available_cpus):
-
+def test_solution(_solution, _deployed_slices, _required_cpus, _total_available_cpus):
+    constraint = 'Constraint'
     consumed_cpu_per_center = np.sum(np.sum(_required_cpus[:, :, np.newaxis] * _solution, axis=0),
                                      axis=0, dtype=np.int16)
-    print(_total_available_cpus)
-    print(consumed_cpu_per_center)
+
+    _print('Total Available CPUs', _total_available_cpus)
+    _print('Consumed CPU Per Center', consumed_cpu_per_center)
 
     # Test if constraint 1 is verified
     verification = 'verified' if np.all(consumed_cpu_per_center <= _total_available_cpus) else 'unverified'
-    Constraint_1 = 'Constraint 1 is ' + verification
+    Constraint_1 = f'\n{constraint} 1 is ' + verification
     print(Constraint_1)
 
-    # Test if constraint 2 is verified
-    print(_solution)
+    # Test if constraint 2 and 3 are verified
+    sums = np.sum(_solution, axis=(1, 2))  # Sum over both rows and columns of each matrix
+
+    # Check if each sum is either 6 or 0
+    verification = 'verified' if np.all(np.logical_or(sums == _solution.shape[1], sums == 0)) else 'unverified'
+    Constraint_2_3 = f'\n{constraint} 2 & 3 are ' + verification
+    print(Constraint_2_3)
+
+    _print('Deployed Slices', _deployed_slices)
+    _print('Solution', _solution)
 
 
 def network_slicing(_number_VNFs, _number_slices, _total_number_centers, _required_cpus, _total_available_cpus):
@@ -59,7 +72,8 @@ def network_slicing(_number_VNFs, _number_slices, _total_number_centers, _requir
     for j in range(_number_slices):
         problem += (pulp.LpAffineExpression([(VNFs_placements[j, i, k], 1)
                                              for i in range(_number_VNFs)
-                                             for k in range(_total_number_centers)]) == _number_VNFs * binary_variables[j],
+                                             for k in range(_total_number_centers)])
+                    == _number_VNFs * binary_variables[j],
                     f'Constraint {constraint}')
         constraint += 1
 
@@ -82,8 +96,9 @@ def main():
     number_slices = 10
     number_VNFs = 6
     required_cpus = generate_random_values(1, 21, (number_slices, number_VNFs))
-    solution, _ = network_slicing(number_VNFs, number_slices, total_number_centers, required_cpus, total_available_cpus)
-    test_solution(solution, required_cpus, total_available_cpus)
+    solution, deployed_slices = network_slicing(number_VNFs, number_slices,
+                                                total_number_centers, required_cpus, total_available_cpus)
+    test_solution(solution, deployed_slices, required_cpus, total_available_cpus)
 
 
 if __name__ == '__main__':
